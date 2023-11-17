36a771c0b40bdc9ef586913f2aad59ac13f6fa72
piktorwa <72793364+piktorwa@users.noreply.github.com>
2023-11-17 14:45:53 +0100

    Update pi_ops.py

--- pi_ops.py
+++ pi_ops.py
@@ -20,16 +20,16 @@ def foo(x):
     c=1
     for hello in range(x):
         if hello % 2 == 0:
-            b += 4 / c#this is a very important operation in calculateing pi according to documentation that is provided in a seperate file in this repository, please analyse this file before using!
+            b += 4 / c
         else:
             b -= 4 / c
         c += 2
         yield b
-    yield 'finished'
-
-def enumerate(pi: pi_container):
-    for hello in pi.a:
-        print(hello)
 
 
-print('All functions are defined')
+def enumerate_pi(pi: pi_container):
+    for my_pi in pi.a:
+        print(my_pi)
+
+if __name__ = "__main__":
+    print('All functions are defined')
