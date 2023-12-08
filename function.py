dcb3a23a0ddb7b06f4d8b57b9c7cb6e31c0eb662
Mateusz Tutaj <mtutaj@student.agh.edu.pl>
2023-12-08 18:02:50 +0100

    Mateusz Tutaj late

---
+++ function.py
@@ -0,0 +1,42 @@
+class PiContainer:
+    def __init__(self, my_pi: list[float] or None = None):
+        if my_pi is None:
+            self.my_pies = []
+        else:
+            self.my_pies = my_pi
+
+    def add_pi(self, next_pies):
+        if isinstance(next_pies, list):
+            self.my_pies += next_pies
+        else:
+            self.my_pies.append(next_pies)
+
+    @property
+    def best(self):
+        return self.my_pies[-1]
+
+
+def calc_pi(approx_level):
+    """
+    result = a+b /c
+    :param approx_level:
+    :return pi
+    """
+    b = 0
+    c = 1
+    for i in range(approx_level):
+        if i % 2 == 0:
+            b += 4 / c
+        else:
+            b -= 4 / c
+        c += 2
+        yield b
+
+
+def enumerate_pi(pi: PiContainer):
+    for my_pi in pi.my_pies:
+        print(my_pi)
+
+
+if __name__ == '__main__':
+    print('All functions are defined')
